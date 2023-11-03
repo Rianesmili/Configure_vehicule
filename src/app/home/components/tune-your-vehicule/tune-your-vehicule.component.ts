87cbb0fd20583247bf20e35f72f66b4a4843c0a3
Rianesmili <rianesmili@gmail.com>
2023-11-03 16:32:14 +0100

    Populate tune your vehicule ts file

--- src/app/home/components/tune-your-vehicule/tune-your-vehicule.component.ts
+++ src/app/home/components/tune-your-vehicule/tune-your-vehicule.component.ts
@@ -1,13 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { UserDataService } from '../../../user-data.service';
 
 @Component({
   selector: 'app-tune-your-vehicule',
   templateUrl: './tune-your-vehicule.component.html',
   styleUrls: ['./tune-your-vehicule.component.scss'],
 })
-export class TuneYourVehiculeComponent  implements OnInit {
+export class TuneYourVehiculeComponent implements OnInit {
 
-  constructor() { }
+  total: number = 0; // Initialisez total à 0
+  selectedItem: string = "";
+  disableTires: boolean = false;
+
+  constructor(private userDataService: UserDataService) {}
+
+  get credits(): number {
+    return this.userDataService.getCredits();
+  }
+
+  set credits(newCredits: number) {
+    this.userDataService.setCredits(newCredits);
+  }
+
+  isTotalExceededAvailableCredits(total: number, availableCredits: number): boolean {
+    return total > availableCredits;
+  }  
+
+  onCheckboxChange(event: CustomEvent) {
+    const isChecked = event.detail.checked;
+    this.disableTires = isChecked && this.selectedItem === 'Nitro';
+  }
 
   ngOnInit() {}
 
