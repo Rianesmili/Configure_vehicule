f2fe773aa7a3b9eb9f71afd6dfb1df73c62863c6
Rianesmili <rianesmili@gmail.com>
2023-11-04 02:36:37 +0100

    add copyPurchaseSummary to purchase component

--- src/app/home/components/purchase/purchase.component.ts
+++ src/app/home/components/purchase/purchase.component.ts
@@ -1,14 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+
+import { Clipboard } from '@capacitor/clipboard';
+
 
 @Component({
   selector: 'app-purchase',
   templateUrl: './purchase.component.html',
   styleUrls: ['./purchase.component.scss'],
 })
-export class PurchaseComponent  implements OnInit {
+export class PurchaseComponent implements OnInit {
 
-  constructor() { }
+  constructor(private clipboard: Clipboard) {
+  }
 
-  ngOnInit() {}
+  total: number = 130;
+  purchasedItems: string = "Car, Soft tires, Nitro (10 units)";
+  creditsLeft: number = 85;
+
+
+  @ViewChild('purchaseSummary', {static: false}) purchaseSummary!: ElementRef;
+
+  copyToClipboard(text: string) {
+    Clipboard.write({
+      string: text
+    }).then(() => {
+      console.log('Texte copié avec succès dans le presse-papiers.');
+    }).catch((error) => {
+      console.error('Erreur lors de la copie du texte dans le presse-papiers :', error);
+    });
+  }
+
+  copyPurchaseSummary() {
+    const purchaseSummaryText = this.purchaseSummary.nativeElement.innerText;
+    this.copyToClipboard(purchaseSummaryText);
+  }
+
+  ngOnInit() {
+  }
 
 }
