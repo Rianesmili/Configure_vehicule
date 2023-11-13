f9785726209cc177aa3acde2996d6152a7f32222
Rianesmili <rianesmili@gmail.com>
2023-11-13 09:20:27 +0100

    Add copy to Clipboard option with toast to mobile device and alert for browser

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { PurchaseComponent } from './home/components/purchase/purchase.component';
 import { TuneYourVehiculeComponent } from './home/components/tune-your-vehicule/tune-your-vehicule.component';
 import {FormsModule} from "@angular/forms";
+import {ClipboardModule} from "ngx-clipboard";
 
 @NgModule({
   declarations: [AppComponent, TuneYourVehiculeComponent, PurchaseComponent],
-    imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FormsModule],
+    imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FormsModule, ClipboardModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/home/components/purchase/purchase.component.ts
+++ src/app/home/components/purchase/purchase.component.ts
@@ -1,6 +1,8 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {Clipboard} from '@capacitor/clipboard';
 import {UserDataService} from "../../repository/user-data.service";
+import { ToastController } from '@ionic/angular';  // service ToastController
+import { Platform } from '@ionic/angular';  // service Platform pour gérer la plateforme si android ou web pour presse papier
 
 @Component({
   selector: 'app-purchase',
@@ -9,7 +11,11 @@ import {UserDataService} from "../../repository/user-data.service";
 })
 export class PurchaseComponent implements OnInit {
 
-  constructor(private userDataService: UserDataService) {
+  constructor(
+    private userDataService: UserDataService,
+    private toastController: ToastController,  // Injectez ToastController
+    private platform: Platform
+  ) {
   }
 
   total: number = 0;
@@ -19,21 +25,46 @@ export class PurchaseComponent implements OnInit {
 
   @ViewChild('purchaseSummary', {static: false}) purchaseSummary!: ElementRef;
 
-  copyToClipboard(text: string) {
-    Clipboard.write({
-      string: text
-    }).then(() => {
-      console.log('Texte copié avec succès dans le presse-papiers.');
-    }).catch((error) => {
-      console.error('Erreur lors de la copie du texte dans le presse-papiers :', error);
+  async copyToClipboard(text: string) {
+    await Clipboard.write({ string: text });
+
+    // Affichez un message de succès basé sur la plateforme
+    if (this.platform.is('cordova') || this.platform.is('capacitor')) {
+      // Code spécifique à Cordova/Capacitor (mobile)
+      this.showToast('Texte copié avec succès');
+    } else {
+      alert('Texte copié avec succès');
+    }
+  }
+
+  async showToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000,
+      position: 'bottom',
     });
+    await toast.present();
   }
 
   copyPurchaseSummary() {
-    if (this.purchaseSummary) {
+    if (this.purchaseSummary && this.purchaseSummary.nativeElement) {
       const purchaseSummaryText = this.purchaseSummary.nativeElement.innerText;
       this.copyToClipboard(purchaseSummaryText);
+    } else {
+      console.error('Aucun élément purchaseSummary ou purchaseSummary.nativeElement trouvé.');
     }
+  }
+
+  getClipboardText(): string {
+    return `Total amount: ${this.total} credits - Purchased: ${this.purchasedItems} - Credits left: ${this.creditsLeft}`;
+  }
+
+  onCopySuccess() {
+    console.log('Texte copié avec succès dans le presse-papiers.');
+  }
+
+  onCopyError() {
+    console.error('Erreur lors de la copie du texte dans le presse-papiers.');
   }
 
   ngOnInit() {
